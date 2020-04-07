f0c452ee0d34be09c56cbcaf3dc86abf65feb019
Ian Ericson <u0636767@gmail.com>
2020-04-07 12:59:19 -0600

    Update Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -17,14 +17,14 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-           
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
-
+     
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -40,6 +40,9 @@ namespace WishList
                 );
         }
 
-
+        private void Configure()
+{
+    throw new NotImplementedException();
+}
     }
 }
