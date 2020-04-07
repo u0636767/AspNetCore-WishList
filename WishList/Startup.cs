4b62e0f6979f7848fd50323984dc7edf6b3306f2
Ian Ericson <u0636767@gmail.com>
2020-04-07 12:53:42 -0600

    Update Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -23,6 +23,7 @@ namespace WishList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+     
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -39,8 +40,8 @@ namespace WishList
         }
 
         private void Configure()
-        {
-            throw new NotImplementedException();
-        }
+{
+    throw new NotImplementedException();
+}
     }
 }
