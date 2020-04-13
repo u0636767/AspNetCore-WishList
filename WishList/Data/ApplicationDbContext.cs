ffba313c0961263288ea452f68d859492fc6ae7f
Ian Ericson <u0636767@gmail.com>
2020-04-13 13:42:16 -0600

    added item view

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
+
+
